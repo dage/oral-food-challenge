573583e5b477b72b6054deda29123c7410d48f4a
dage <dag.erlandsen@gmail.com>
2022-07-24 19:06:03 +0200

    Initial version.

---
+++ src/components/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('dummy test', () => {
+    expect(true);
+//    render(<App />);
+//    const linkElement = screen.getByText(/learn react/i);
+//    expect(linkElement).toBeInTheDocument();
+});
